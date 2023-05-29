156882b4397c3c6792c4f4fa5424054537077888
Ilija Eftimov <ieftimov@pm.me>
2023-05-29 20:17:08 +0200

    Add loading spinner

--- app/static/application.js
+++ app/static/application.js
@@ -1,17 +1,19 @@
 const toastTrigger = document.getElementById('liveToastBtn')
 const toastLiveExample = document.getElementById('liveToast')
 const toastBody = document.getElementById('toast-body')
+const loadingSpinner = document.getElementById('loadingSpinner')
 
 if (toastTrigger) {
   const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
-  console.log(toastBootstrap);
   toastTrigger.addEventListener('click', async () => {
+    loadingSpinner.removeAttribute("hidden");
     let slug = event.target.getAttribute('data-slug');
     fetch("/summarize/" + slug)
       .then((response) => response.text())
       .then((data) => {
         toastBody.innerHTML = data
         toastBootstrap.show()
+        loadingSpinner.setAttribute("hidden", "true");
       })
   })
 }
